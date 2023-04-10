99cff468848f78e666498732211455ddaea4e1d6
noah-clements <noahclements@gmail.com>
2023-04-09 23:25:14 -0400

    implemented other methods

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,6 +8,12 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
@@ -15,10 +21,20 @@ fn main() {
         width: 30, 
         height: 50 
     };
-    dbg!(&rect1);
+    let rect2 = Rectangle { 
+        width: 10, 
+        height: 40 
+    };
+    let rect3 = Rectangle { 
+        width: 60, 
+        height: 45 
+    };
+
+    let square = Rectangle::square(30);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Can rect1 hold square? {}", rect1.can_hold(&square));
+    println!("The area of rect1 is {} square pixels.", rect1.area());
     // println!("rect1 is {:#?}", rect1);
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
 }
