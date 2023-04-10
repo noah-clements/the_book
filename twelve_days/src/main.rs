097f0ca2ceba1b961c48ebffc44b8d708cf707a4
noah-clements <noahclements@gmail.com>
2023-04-09 22:56:47 -0400

    used days enumerator

--- twelve_days/src/main.rs
+++ twelve_days/src/main.rs
@@ -4,17 +4,16 @@ fn main() {
     let days = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
     let gifts = ["a partridge in a pear tree", "two turtle doves", "three french hens", "four calling birds", "five golden rings", "six geese a-laying", "seven swans a-swimming", "eight maids a-milking", "nine ladies dancing", "ten lords a-leaping", "eleven pipers piping", "twelve drummers drumming"];
     // let mut day = 0;
-    for day in 0..12 {
-        println!("On the {} day of Christmas my true love gave to me", days[day]);
+    for (day_num, day) in days.iter().enumerate() {
+        println!("On the {} day of Christmas my true love gave to me", day);
 
-        for gift in (0..day+1).rev(){
-            if day > 0 && gift == 0 {
+        for gift in (0..day_num+1).rev(){
+            if day_num > 0 && gift == 0 {
                 println!("And {}", gifts[gift]);
                 continue;
             } else {
                 println!("{}", gifts[gift]);
             }
         }
-        println!("");
     }
 }
