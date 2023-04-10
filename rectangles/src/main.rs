4d5108953112c683af4586a115aeb850aeb21258
noah-clements <noahclements@gmail.com>
2023-04-09 23:12:15 -0400

    v4 - using dbg! macro

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,10 +1,17 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
+    let scale = 2;
+    let rect1 = Rectangle { 
+        width: dbg!(30 * scale), 
+        height: 50 
+    };
+    dbg!(&rect1);
+    // println!("rect1 is {:#?}", rect1);
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
